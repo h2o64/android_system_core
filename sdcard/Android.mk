aa549923a4a3f01257c24c71b44b14081ca6c532
Ameya Thakur <ameyat@codeaurora.org>
2016-03-01 16:39:43 -0800

    sdcard: Add support for the fuse passthrough extensions
    We now support the fuse passthrough driver in the sdcard daemon.
    
    Change-Id: I3a5682678df55f77b296eb257bb4f9567bd2b061

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -5,7 +5,6 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 endif
-
 LOCAL_SRC_FILES := sdcard.c
 LOCAL_MODULE := libsdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror
